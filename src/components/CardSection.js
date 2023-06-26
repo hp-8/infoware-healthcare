84df3f0094232581f40e5c69ad763ac9172df838
H P <106737575+hp-8@users.noreply.github.com>
2023-06-26 17:36:29 +0530

    CardSection made responsive

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import './CardSection.css'
+import './CardSection.css';
 
 const CardSection = () => {
   return (
-    <section className="col-2-cards cards-main-container border-color--purple module-container" aria-labelledby="cards-module-heading-BA25C5B487FF8E8E440B1592C076C573">
+    <section className="cards-main-container border-color--purple module-container" aria-labelledby="cards-module-heading-BA25C5B487FF8E8E440B1592C076C573">
       <div className="contain module-wrap">
-
         <div className="module-header module-content">
           <h2 className="h2" id="cards-module-heading-BA25C5B487FF8E8E440B1592C076C573">
             <span role="text">
-              <span className="content-label">Pediatric Care </span>
+              <span className="content-label">Pediatric Care</span>
               Forward. For Our Children. For All Children
             </span>
           </h2>
-
           <p className="cards-description">Learn how the experts at Johns Hopkins All Children’s Hospital and Johns Hopkins Children's Center push the boundaries of pediatric medicine to make it possible for patients and families to fulfill those dreams.</p>
         </div>
-
-        <ul className="cards-container col-2">
-          <li className="col-2-item card-has-image card">
+        <ul className="cards-container">
+          <li className="card col-2-item card-has-image">
             <div className="card-content">
-            <div className="card-media flex-wrapper">
+              <div className="card-media">
                 <img className="loading" src="https://www.hopkinsmedicine.org/johns-hopkins-childrens-center/_images/homepage/Mahi-slider-cropped.jpg" alt="Mahi playing doctor with her parent" width="80" height="80" data-was-processed="true" />
               </div>
               <div className="content">
-          
                 <a href="https://www.hopkinsmedicine.org/forward/jhach" className="card-link">
                   <span className="h3">Johns Hopkins All Children's Hospital</span>
                   <span className="arrow-container" aria-hidden="true">
@@ -34,14 +30,11 @@ const CardSection = () => {
                   </span>
                 </a>
               </div>
-
-             
             </div>
           </li>
-
-          <li className="col-2-item card-has-image card">
+          <li className="card col-2-item card-has-image">
             <div className="card-content">
-            <div className="card-media flex-wrapper">
+              <div className="card-media">
                 <img className="loading" src="https://www.hopkinsmedicine.org/johns-hopkins-childrens-center/_images/homepage/Lem-card-image.jpg" alt="Lem playing with an astronaut helmet" width="80" height="80" data-was-processed="true" />
               </div>
               <div className="content">
@@ -54,12 +47,9 @@ const CardSection = () => {
                   </span>
                 </a>
               </div>
-
-
             </div>
           </li>
         </ul>
-
       </div>
     </section>
   );
