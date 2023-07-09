ede0154a46193db75e28bd6a69ff5d1e07d77a3a
H P <106737575+hp-8@users.noreply.github.com>
2023-07-10 00:23:00 +0530

    the last one

--- src/App.js
+++ src/App.js
@@ -10,27 +10,41 @@ import Doctors from './pages/Doctors';
 import Slider from './components/Slider';
 import Appointments from './pages/appointment';
 import AppointmentForm from './pages/appointmentForm';
-import Location from './pages/location'
+import Location from './pages/location';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import './App.css';
 
 const ConditionalComponents = () => {
-const location = useLocation();
+  const location = useLocation();
   const { pathname } = location;
-  
-  if (pathname === '/Doctors' || pathname === '/Appointments' || pathname === '/appointmentform' || pathname === '/Location') {
-    return null; 
+
+  if (
+    pathname === '/Doctors' ||
+    pathname === '/Appointments' ||
+    pathname === '/appointmentform' ||
+    pathname === '/Location'
+  ) {
+    return null;
   }
-  
+
   return (
     <>
-      <Slider/>
+      <Slider />
       <Guidelines />
       <PaymentSection />
       <CarouselContent />
       <CardSection />
+    </>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <Slider />
+      <InfoCard />
     </>
   );
 };
@@ -41,11 +55,11 @@ const App = () => {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<InfoCard />} />
+          <Route path="/" element={<Home />} />
           <Route path="/Doctors" element={<Doctors />} />
-          <Route path="/Appointments" element= {<Appointments/>}/>
-          <Route path="/appointmentform" element = {<AppointmentForm/>}/>
-          <Route path="/Location" element = {<Location/>}/>
+          <Route path="/Appointments" element={<Appointments />} />
+          <Route path="/appointmentform" element={<AppointmentForm />} />
+          <Route path="/Location" element={<Location />} />
         </Routes>
         <ConditionalComponents />
       </div>
